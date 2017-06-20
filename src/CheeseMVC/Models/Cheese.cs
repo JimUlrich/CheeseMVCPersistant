589c49f949348d607db32ae7094b070966de9edb
Jim Ulrich <jupagblkxten@gmail.com>
2017-06-19 21:39:56 -0500

    finished part 2

---
+++ src/CheeseMVC/Models/Cheese.cs
@@ -0,0 +1,14 @@
+﻿namespace CheeseMVC.Models
+{
+    public class Cheese
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int ID { get; set; }
+
+        public CheeseCategory Category { get; set; }
+        public int CategoryID { get; set; }
+
+
+    }
+}
